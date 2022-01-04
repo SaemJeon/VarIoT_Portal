5bb88ecdbdd9e8f9be78b10de3760e261cfc49e4
SaemJeon <38971167+saemjeon@users.noreply.github.com>
2022-01-03 21:15:47 -0500

    Adding base template & add device page

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # VarIoT_Portal
+
+## Prerequisite
+1. Install Flask
+2. Anaconda
+
+## Run
+```
+export FLASK_APP=run
+export FLASK_env=development
+flask run
+```
--- run.py
+++ run.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="public", static_url_path="")
 
 @app.route("/")
-def hello_world():
-  return "Welcome to VarIoT"
+def index():
+  return render_template("index.html")
+
+@app.route("/add_device", methods=["GET", "POST"])
+def add_device():
+  if request.method == "POST":
+    name = request.form["name"]
+    type = request.form["type"]
+    print(name, type)
+  return render_template("add_device.html")
 
 app.run('127.0.0.1', port = 5500, debug = True)
